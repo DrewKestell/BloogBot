29bb4a34a7b87fb2a3b6a4dfe8c120ecd4d3bf7a
Moandor <moandor.y@gmail.com>
2026-03-09 22:05:57 -0500

    ProtectionPaladinBot/MoveToTargetState: check for stuckness

--- ProtectionPaladinBot/MoveToTargetState.cs
+++ ProtectionPaladinBot/MoveToTargetState.cs
@@ -2,6 +2,7 @@
 using BloogBot.AI;
 using BloogBot.Game;
 using BloogBot.Game.Objects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -15,6 +16,8 @@ namespace ProtectionPaladinBot
         readonly LocalPlayer player;
         readonly StuckHelper stuckHelper;
 
+        int stateStartTime;
+
         internal MoveToTargetState(Stack<IBotState> botStates, IDependencyContainer container, WoWUnit target)
         {
             this.botStates = botStates;
@@ -22,12 +25,25 @@ namespace ProtectionPaladinBot
             this.target = target;
             player = ObjectManager.Player;
             stuckHelper = new StuckHelper(botStates, container);
+
+            stateStartTime = Environment.TickCount;
         }
 
         public void Update()
         {
             if (target.TappedByOther || (ObjectManager.Aggressors.Count() > 0 && !ObjectManager.Aggressors.Any(a => a.Guid == target.Guid)))
             {
+                player.StopAllMovement();
+                botStates.Pop();
+                return;
+            }
+
+            // If we've been trying to move to the target for more than 30 seconds, we're probably
+            // stuck.
+            if (Environment.TickCount - stateStartTime > 30 * 1000)
+            {
+                // Add the target to the in-memory blacklist and stop trying to fight it.
+                container.Probe.BlacklistedMobIds.Add(target.Guid);
                 player.StopAllMovement();
                 botStates.Pop();
                 return;
